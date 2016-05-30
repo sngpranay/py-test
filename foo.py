664c7655ccba240a7771d2530b4bf5155812a782
Pranay Singh <sng.pranay@gmail.com>
2016-05-30 18:09:28 +0530

    added files

---
+++ foo.py
@@ -0,0 +1,5 @@
+class test(object):
+	def __init__(self):
+		pass
+
+	def add(self)
---
+++ shell-test.sh
@@ -0,0 +1,6 @@
+
+. ~/Desktop/Selenium_Test/my_tests/bin/activate
+python ~/Desktop/Selenium_Test/my_tests/jedi.py
+
+
+
